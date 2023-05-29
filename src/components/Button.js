f0699c0c9efbb355f68a80b1e1daaed7784d7f7b
Jvfrango <jv_franco@outlook.com>
2023-05-29 11:16:17 -0300

    fix btn

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,7 @@
 export function Button({ props }) {
   return (
     <a
-      className="w-full flex items-center justify-center"
+      className=" flex items-center justify-center"
       href="https://wa.me/message/OKMV4RBZS26PG1"
     >
       <button className="btn uppercase ">{props}</button>
